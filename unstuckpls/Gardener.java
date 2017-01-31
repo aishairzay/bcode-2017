2206204e7ed273b5a73de16cd20ebc4dbe3386c9
Amit Ishairzay <amit.ishairzay@gmail.com>
2017-01-31 05:26:10 -0500

    Reduce bullet threshhold to buy extra soldiers.

--- unstuckpls/Gardener.java
+++ unstuckpls/Gardener.java
@@ -175,7 +175,7 @@ public strictfp class Gardener extends Bot {
 				rc.donate(toDonate);
 			}
 		} else {
-			if (rc.getTeamBullets() <= 150) {
+			if (rc.getTeamBullets() <= 135) {
 				return;
 			}
 			this.buildUnit(RobotType.SOLDIER);
